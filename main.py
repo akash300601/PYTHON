7191544a9653bab59d1171456c182fd0f3619698
akash300601 <akash30062001@gmail.com>
2024-05-05 22:29:52 +0530

    Main program

---
+++ main.py
@@ -0,0 +1,28 @@
+
+
+
+# Python program to demonstrate
+# main() function
+
+
+print("Hello")
+
+
+# Defining main function
+print('Helloworld')
+
+print("Selva", 323, 3232.323, False)
+print()
+# print statement prints text in a new line
+# The value of end is appended to the output and the next print will continue from here.
+print("Hello", end="")
+print("world")
+print("Hello", end=" ")
+print("world")
+def test():
+        print("Hello from test")
+
+def main():
+    test()
+    print("Hello for Main")
+main()
